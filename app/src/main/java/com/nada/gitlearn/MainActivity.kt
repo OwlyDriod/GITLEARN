7446c1f6224f70d5b54d7dc815dc4046fa93a1c0
nada el kalai <nada.elkalai@gmail.com>
2021-08-15 01:15:12 +0100

    add name

--- app/src/main/java/com/nada/gitlearn/MainActivity.kt
+++ app/src/main/java/com/nada/gitlearn/MainActivity.kt
@@ -2,10 +2,14 @@ package com.nada.gitlearn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var name:String ="nada"
+        Log.d("tast","msg name $name")
     }
 }
